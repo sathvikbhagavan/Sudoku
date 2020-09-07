0cfcd5af60480bc49f1e5c0f782432518f984c05
sathvikbhagavan <35105271+sathvikbhagavan@users.noreply.github.com>
2020-09-07 11:45:51 +0530

    Code Refactor

--- get_grid.py
+++ get_grid.py
@@ -19,23 +19,23 @@ def find_puzzle(image, debug=False):
 	cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 	cnts = imutils.grab_contours(cnts)
 	cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
-	puzzleCnt = None
+	puzzle_cnt = None
 	for c in cnts:
 		peri = cv2.arcLength(c, True)
 		approx = cv2.approxPolyDP(c, 0.02 * peri, True)
 		if len(approx) == 4:
-			puzzleCnt = approx
+			puzzle_cnt = approx
 			break
-	if puzzleCnt is None:
+	if puzzle_cnt is None:
 		raise Exception(("Could not find Sudoku puzzle outline. "
 			"Try debugging your thresholding and contour steps."))
 	if debug:
 		output = image.copy()
-		cv2.drawContours(output, [puzzleCnt], -1, (0, 255, 0), 2)
+		cv2.drawContours(output, [puzzle_cnt], -1, (0, 255, 0), 2)
 		cv2.imshow("Puzzle Outline", output)
 		cv2.waitKey(0)
-	puzzle = four_point_transform(image, puzzleCnt.reshape(4, 2))
-	warped = four_point_transform(gray, puzzleCnt.reshape(4, 2))
+	puzzle = four_point_transform(image, puzzle_cnt.reshape(4, 2))
+	warped = four_point_transform(gray, puzzle_cnt.reshape(4, 2))
 	if debug:
 		cv2.imshow("Puzzle Transform", puzzle)
 		cv2.waitKey(0)
@@ -57,8 +57,8 @@ def extract_digit(cell, debug=False):
 	mask = np.zeros(thresh.shape, dtype="uint8")
 	cv2.drawContours(mask, [c], -1, 255, -1)
 	(h, w) = thresh.shape
-	percentFilled = cv2.countNonZero(mask) / float(w * h)
-	if percentFilled < 0.03:
+	percent_filled = cv2.countNonZero(mask) / float(w * h)
+	if percent_filled < 0.03:
 		return None
 	digit = cv2.bitwise_and(thresh, thresh, mask=mask)
 	if debug:
@@ -69,7 +69,7 @@ def extract_digit(cell, debug=False):
 
 # Main calling function
 # Once the digits are found, using ocr for classifying the digits
-def get_grid(image_path, model_path, number_of_models):
+def get_grid(image_path, model_path, number_of_models, debug):
 	print("[INFO] loading digit classifier...")
 	models = []
 	for i in range(number_of_models):
@@ -77,21 +77,21 @@ def get_grid(image_path, model_path, number_of_models):
 	print("[INFO] processing image...")
 	image = cv2.imread(image_path)
 	image = imutils.resize(image, width=600)
-	(puzzleImage, warped) = find_puzzle(image, debug=0)
+	(puzzle_image, warped) = find_puzzle(image, debug)
 	board = np.zeros((9, 9), dtype="int")
-	stepX = warped.shape[1] // 9
-	stepY = warped.shape[0] // 9
-	cellLocs = []
+	step_x = warped.shape[1] // 9
+	step_y = warped.shape[0] // 9
+	cell_locs = []
 	for y in range(0, 9):
 		row = []
 		for x in range(0, 9):
-			startX = x * stepX
-			startY = y * stepY
-			endX = (x + 1) * stepX
-			endY = (y + 1) * stepY
-			row.append((startX, startY, endX, endY))
-			cell = warped[startY:endY, startX:endX]
-			digit = extract_digit(cell, debug=0)
+			start_x = x * step_x
+			start_y = y * step_y
+			end_x = (x + 1) * step_x
+			end_y = (y + 1) * step_y
+			row.append((start_x, start_y, end_x, end_y))
+			cell = warped[start_y:end_y, start_x:end_x]
+			digit = extract_digit(cell, debug)
 			if digit is not None:
 				roi = cv2.resize(digit, (28, 28))
 				roi = roi.astype("float") / 255.0
@@ -103,6 +103,6 @@ def get_grid(image_path, model_path, number_of_models):
 				pred = pred.argmax(axis=1)[0]
 				print(pred)
 				board[y, x] = pred
-		cellLocs.append(row)
+		cell_locs.append(row)
 	print("[INFO] OCR'd Sudoku board:")
 	return board
